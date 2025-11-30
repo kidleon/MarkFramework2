45cee720866abda71dc5a1dfbcbfd13f90a52869
KIDLEON-HOMEPC\kidle <kidleon@gmail.com>
2025-11-30 13:25:46 +0900

    Template memory allocator 개선

--- engine/mark3d/generic/TAllocator.cpp
+++ engine/mark3d/generic/TAllocator.cpp
@@ -10,98 +10,104 @@
 
 namespace mark
 {
-	void* talloc(
+	void* talloc_syscall(
 		MEM_SIZE size,
-		ALLOC_TYPE alloc_type,
 		const char* file,
 		int line,
 		const char* func
 	) 
 	{
-		switch (alloc_type)
-		{
-			case ALLOC_TYPE::SYSCALL:
-				return MemoryManager::Alloc_SysCall(size, file, line, func);
-
-			case ALLOC_TYPE::POOL:
-				return MemoryManager::AllocPool(size, file, line, func);
-
-			case ALLOC_TYPE::TEMP:
-				return MemoryManager::AllocTemp(size);
-		}
-
-		return nullptr;
+		return MemoryManager::Alloc_SysCall(size, file, line, func);
 	}
 
-	void* trealloc(
-		void* ptr,
-		MEM_SIZE old_size,
-		MEM_SIZE new_size,
-		ALLOC_TYPE alloc_type,
+	void* talloc_pool(
+		MEM_SIZE size,
 		const char* file,
 		int line,
 		const char* func
 	)
 	{
-		switch (alloc_type)
-		{
-			case ALLOC_TYPE::SYSCALL:
-			{
-				return MemoryManager::Realloc_Syscall(ptr, new_size, file, line, func);
-			}
-
-			case ALLOC_TYPE::POOL:
-			{
-				void* new_ptr = MemoryManager::AllocPool(new_size, file, line, func);
-				if (!new_ptr)
-					return nullptr;
-
-				if (ptr)
-				{
-					memcpy(new_ptr, ptr, old_size);
-					MemoryManager::FreePool(ptr);
-				}
-
-				return new_ptr;
-			}
-
-			case ALLOC_TYPE::TEMP:
-			{
-				void* new_ptr = MemoryManager::AllocTemp(new_size);
-				if (!new_ptr)
-					return nullptr;
-
-				if (ptr)
-				{
-					memcpy(new_ptr, ptr, old_size);
-					// Temp 방식은 기존 포인터를 해제하지 않음
-				}
-
-				return new_ptr;
-			}
-		}
-
-		return nullptr;
+		return MemoryManager::AllocPool(size, file, line, func);
 	}
 
-	void tfree(
-		void* ptr,
-		ALLOC_TYPE alloc_type
+	void* talloc_temp(
+		MEM_SIZE size,
+		const char* file,
+		int line,
+		const char* func
 	)
 	{
-		switch (alloc_type)
+		return MemoryManager::AllocTemp(size);
+	}
+
+	void* trealloc_syscall(
+		void* ptr,
+		MEM_SIZE old_size,
+		MEM_SIZE new_size,
+		const char* file,
+		int line,
+		const char* func
+	)
+	{
+		return MemoryManager::Realloc_Syscall(ptr, new_size, file, line, func);
+	}
+
+	void* trealloc_pool(
+		void* ptr,
+		MEM_SIZE old_size,
+		MEM_SIZE new_size,
+		const char* file,
+		int line,
+		const char* func
+	)
+	{
+		void* new_ptr = MemoryManager::AllocPool(new_size, file, line, func);
+		if (!new_ptr)
+			return nullptr;
+		if (ptr)
 		{
-			case ALLOC_TYPE::SYSCALL:
-				MemoryManager::Free_SysCall(ptr);
-				break;
-
-			case ALLOC_TYPE::POOL:
-				MemoryManager::FreePool(ptr);
-				break;
-
-			case ALLOC_TYPE::TEMP:
-				// Temp 방식은 개별 해제를 지원하지 않음
-				break;
+			memcpy(new_ptr, ptr, old_size);
+			MemoryManager::FreePool(ptr);
 		}
+		return new_ptr;
+	}
+
+	void* trealloc_temp(
+		void* ptr,
+		MEM_SIZE new_size,
+		const char* file,
+		int line,
+		const char* func
+	)
+	{
+		void* new_ptr = MemoryManager::AllocTemp(new_size);
+		if (!new_ptr)
+			return nullptr;
+		if (ptr)
+		{
+			// Temp 방식은 기존 포인터를 해제하지 않음
+		}
+		return new_ptr;
+	}
+
+	void tfree_syscall(
+		void* ptr
+	)
+	{
+		MemoryManager::Free_SysCall(ptr);
+	}
+
+	void tfree_pool(
+		void* ptr
+	)
+	{
+		MemoryManager::FreePool(ptr);
+	}
+
+	void tfree_temp(
+		void* ptr
+	)
+	{
+		// Temp 방식은 개별 해제를 지원하지 않음
 	}
 }
--- engine/mark3d/generic/TAllocator.h
+++ engine/mark3d/generic/TAllocator.h
@@ -12,57 +12,175 @@ namespace mark
 	};
 
 	/**
-	* @brief 템플릿 메모리 할당 함수
+	* @brief 템플릿 시스템 메모리 할당 함수
 	* @param size 할당할 메모리 크기
-	* @param alloc_type 할당 유형
 	* @param file 할당 요청이 발생한 소스 파일 이름
 	* @param line 할당 요청이 발생한 소스 파일의 라인 번호
 	* @param func 할당 요청이 발생한 함수 이름
 	* @return 할당된 메모리의 포인터, 실패시 nullptr
 	*/
-	MARKENGINE_C_API void* talloc(
+	MARKENGINE_C_API void* talloc_syscall(
 		MEM_SIZE size,
-		ALLOC_TYPE alloc_type,
 		const char* file,
 		int line,
 		const char* func
 	);
 
 	/**
-	* @brief 템플릿 메모리 재할당 함수
+	* @brief 템플릿 풀 메모리 할당 함수
+	* @param size 할당할 메모리 크기
+	* @param file 할당 요청이 발생한 소스 파일 이름
+	* @param line 할당 요청이 발생한 소스 파일의 라인 번호
+	* @param func 할당 요청이 발생한 함수 이름
+	* @return 할당된 메모리의 포인터, 실패시 nullptr
+	*/
+	MARKENGINE_C_API void* talloc_pool(
+		MEM_SIZE size,
+		const char* file,
+		int line,
+		const char* func
+	);
+
+	/**
+	* @brief 템플릿 임시 메모리 할당 함수
+	* @param size 할당할 메모리 크기
+	* @param file 할당 요청이 발생한 소스 파일 이름
+	* @param line 할당 요청이 발생한 소스 파일의 라인 번호
+	* @param func 할당 요청이 발생한 함수 이름
+	* @return 할당된 메모리의 포인터, 실패시 nullptr
+	*/
+	MARKENGINE_C_API void* talloc_temp(
+		MEM_SIZE size,
+		const char* file,
+		int line,
+		const char* func
+	);
+
+	/**
+	* @brief 템플릿 시스템 메모리 재할당 함수
 	* @param ptr 재할당할 메모리의 기존 포인터
 	* @param old_size 재할당 전 메모리 크기
 	* @param new_size 재할당할 메모리 크기
-	* @param alloc_type 할당 유형
 	* @param file 재할당 요청이 발생한 소스 파일 이름
 	* @param line 재할당 요청이 발생한 소스 파일의 라인 번호
 	* @param func 재할당 요청이 발생한 함수 이름
 	* @return 재할당된 메모리의 포인터, 실패시 nullptr
 	*/
-	MARKENGINE_C_API void* trealloc(
+	MARKENGINE_C_API void* trealloc_syscall(
 		void* ptr,
 		MEM_SIZE old_size,
 		MEM_SIZE new_size,
-		ALLOC_TYPE alloc_type,
+		const char* file,
+		int line,
+		const char* func
+	);
+
+	MARKENGINE_C_API void* trealloc_pool(
+		void* ptr,
+		MEM_SIZE old_size,
+		MEM_SIZE new_size,
+		const char* file,
+		int line,
+		const char* func
+	);
+
+	MARKENGINE_C_API void* trealloc_temp(
+		void* ptr,
+		MEM_SIZE new_size,
 		const char* file,
 		int line,
 		const char* func
 	);
 
 	/**
-	* @brief 템플릿 메모리 해제 함수
+	* @brief 템플릿 시스템 메모리 해제 함수
 	* @param ptr 해제할 메모리의 포인터
-	* @param alloc_type 할당 유형
 	* @return 없음
 	*/
-	MARKENGINE_C_API void tfree(
-		void* ptr,
-		ALLOC_TYPE alloc_type
+	MARKENGINE_C_API void tfree_syscall(
+		void* ptr
 	);
+
+	/**
+	* @brief 템플릿 풀 메모리 해제 함수
+	* @param ptr 해제할 메모리의 포인터
+	* @return 없음
+	*/
+	MARKENGINE_C_API void tfree_pool(
+		void* ptr
+	);
+
+	/**
+	* @brief 템플릿 임시 메모리 해제 함수
+	* @param ptr 해제할 메모리의 포인터
+	* @return 없음
+	*/
+	MARKENGINE_C_API void tfree_temp(
+		void* ptr
+	);
+
+
+
+	template<ALLOC_TYPE _AllocType>
+	struct TAlloc_Syscall
+	{
+		static inline void* alloc(MEM_SIZE size, const char* file, int line, const char* func)
+		{
+			if constexpr(ALLOC_TYPE::SYSCALL == _AllocType)
+			{
+				return mark::talloc(size, file, line, func);
+			}
+			else if constexpr(ALLOC_TYPE::POOL == _AllocType)
+			{
+				return mark::talloc(size, file, line, func);
+			}
+			else if constexpr(ALLOC_TYPE::TEMP = _AllocType)
+			{
+				return mark::talloc(size, file, line, func);
+			}
+
+			return nullptr;
+		}
+
+		static inline void* realloc(void* ptr, MEM_SIZE old_size, MEM_SIZE new_size, const char* file, int line, const char* func)
+		{
+			if constexpr(ALLOC_TYPE::SYSCALL == _AllocType)
+			{
+				return mark::trealloc(ptr, old_size, new_size, file, line, func);
+			}
+			else if constexpr(ALLOC_TYPE::POOL == _AllocType)
+			{
+				return mark::trealloc(ptr, old_size, new_size, file, line, func);
+			}
+			else if constexpr(ALLOC_TYPE::TEMP = _AllocType)
+			{
+				return mark::trealloc(ptr, new_size, file, line, func);
+			}
+			return nullptr;
+			
+		}
+
+		static inline void free(void* ptr)
+		{
+			if constexpr(ALLOC_TYPE::SYSCALL == _AllocType)
+			{
+				mark::tfree_syscall(ptr);
+			}
+			else if constexpr(ALLOC_TYPE::POOL == _AllocType)
+			{
+				mark::tfree_pool(ptr);
+			}
+			else if constexpr(ALLOC_TYPE::TEMP = _AllocType)
+			{
+				mark::tfree_temp(ptr);
+			}
+			
+		}
+	};
 }
 
-#	define GENERIC_ALLOC(size, alloc_type) mark::talloc(size, alloc_type, __FILE__, __LINE__, __FUNCTION__)
-#	define GENERIC_REALLOC(ptr, old_size, new_size, alloc_type) mark::trealloc(ptr, old_size, new_size, alloc_type, __FILE__, __LINE__, __FUNCTION__)
-#	define GENERIC_FREE(ptr, alloc_type) mark::tfree(ptr, alloc_type)
+#	define GENERIC_ALLOC(size, alloc_type) mark::TAlloc_Syscall<alloc_type>::alloc(size, __FILE__, __LINE__, __FUNCTION__)
+#	define GENERIC_REALLOC(ptr, old_size, new_size, alloc_type) mark::TAlloc_Syscall<alloc_type>::realloc(ptr, old_size, new_size, __FILE__, __LINE__, __FUNCTION__)
+#	define GENERIC_FREE(ptr, alloc_type) mark::TAlloc_Syscall<alloc_type>::free(ptr)
 
 #endif // __GENERIC_ALLOCATOR_H__
